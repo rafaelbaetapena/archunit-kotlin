b55ed6f1a353bac6cae7179407c92e6a030b88bc
Rafael Baeta Pena <rafaelrbncp@gmail.com>
2023-04-15 19:04:07 -0300

    initial project

---
+++ src/main/kotlin/com/rafaelbaetapena/archunitkotlin/ArchunitKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.rafaelbaetapena.archunitkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ArchunitKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<ArchunitKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/rafaelbaetapena/archunitkotlin/ArchunitKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.rafaelbaetapena.archunitkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ArchunitKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
