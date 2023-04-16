1bdf9d99105d520c2744352e83b14e4a37c54931
Rafael Baeta Pena <rafaelrbncp@gmail.com>
2023-04-16 12:27:48 -0300

    add port.in and port.out

--- src/test/kotlin/com/rafaelbaetapena/archunitkotlin/architecture/PackagesConstants.kt
+++ src/test/kotlin/com/rafaelbaetapena/archunitkotlin/architecture/PackagesConstants.kt
@@ -4,6 +4,8 @@ object PackagesConstants {
     const val configPackageName: String = "..config"
     const val corePackageName: String = "..core"
     const val corePortPackageName: String = "..core.port"
+    const val corePortInPackageName: String = "..core.port.in"
+    const val corePortOutPackageName: String = "..core.port.out"
     const val dataProviderPackageName: String = "..dataprovider"
     const val entrypointPackageName: String = "..entrypoint"
 }
--- src/test/kotlin/com/rafaelbaetapena/archunitkotlin/architecture/core/PortRules.kt
+++ src/test/kotlin/com/rafaelbaetapena/archunitkotlin/architecture/core/PortRules.kt
@@ -3,10 +3,23 @@ package com.rafaelbaetapena.archunitkotlin.architecture.core
 import com.rafaelbaetapena.archunitkotlin.architecture.PackagesConstants
 import com.tngtech.archunit.junit.ArchTest
 import com.tngtech.archunit.lang.ArchRule
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes
 import com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses
 
 class PortRules {
     @ArchTest
     val noClassesShouldResideInAPackageCorePort: ArchRule =
         noClasses().should().resideInAPackage(PackagesConstants.corePortPackageName)
+
+    @ArchTest
+    val classesThatResideInAPackagePortInShouldBeInterfaces: ArchRule =
+        classes().that().resideInAPackage(PackagesConstants.corePortInPackageName)
+            .should().beInterfaces()
+            .andShould().haveSimpleNameEndingWith("PortIn")
+
+    @ArchTest
+    val classesThatResideInAPackagePortOutShouldBeInterfaces: ArchRule =
+        classes().that().resideInAPackage(PackagesConstants.corePortOutPackageName)
+            .should().beInterfaces()
+            .andShould().haveSimpleNameEndingWith("PortOut")
 }
