47f2dcd5299cf8e6dc2e7488b67c8485b7be68c9
Rafael Baeta Pena <rafaelrbncp@gmail.com>
2023-04-16 11:55:36 -0300

    package DataProvider test package Entrypoint test

--- src/test/kotlin/com/rafaelbaetapena/archunitkotlin/architecture/ArchitectureTest.kt
+++ src/test/kotlin/com/rafaelbaetapena/archunitkotlin/architecture/ArchitectureTest.kt
@@ -3,7 +3,8 @@ package com.rafaelbaetapena.archunitkotlin.architecture
 import com.tngtech.archunit.core.importer.ImportOption.*
 import com.tngtech.archunit.junit.AnalyzeClasses
 import com.tngtech.archunit.junit.ArchTest
-import com.tngtech.archunit.lang.syntax.ArchRuleDefinition.*
+import com.tngtech.archunit.lang.ArchRule
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition
 
 @AnalyzeClasses(
     importOptions = [
@@ -14,7 +15,28 @@ import com.tngtech.archunit.lang.syntax.ArchRuleDefinition.*
     packagesOf = [ArchitectureTest::class]
 )
 class ArchitectureTest {
+
+    // Config Package Rules
     @ArchTest
-    val noClassesShouldResideInAPackageConfig =
-        noClasses().should().resideInAPackage(PackagesConstants.config)
+    val noClassesShouldResideInAPackageConfig: ArchRule =
+        ArchRuleDefinition.noClasses().should().resideInAPackage(configPackageName)
+
+    // DataProvider Package Rules
+    @ArchTest
+    val noClassesShouldResideInAPackageDataProvider: ArchRule =
+        ArchRuleDefinition.noClasses().should().resideInAPackage(dataProviderPackageName)
+
+    // Entrypoint Package Rules
+    @ArchTest
+    val noClassesShouldResideInAPackageEntrypoint: ArchRule =
+        ArchRuleDefinition.noClasses().should().resideInAPackage(entrypointPackageName)
+
+    companion object {
+        const val configPackageName: String = "..config"
+        const val dataProviderPackageName: String = "..dataprovider"
+        const val entrypointPackageName: String = "..entrypoint"
+    }
+
+
+
 }
--- src/test/kotlin/com/rafaelbaetapena/archunitkotlin/architecture/PackagesConstants.kt
+++
@@ -1,6 +0,0 @@
-package com.rafaelbaetapena.archunitkotlin.architecture
-
-object PackagesConstants {
-    val config: String = "..config"
-    val configDeep: String = "..config.."
-}
