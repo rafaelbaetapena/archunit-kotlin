a178a7e9ee217069ef5990a4d60e9f8f3c3c0847
Rafael Baeta Pena <rafaelrbncp@gmail.com>
2023-04-16 17:38:39 -0300

    adding archunit_ignore_patterns.txt

--- src/test/kotlin/com/rafaelbaetapena/archunitkotlin/architecture/PackagesConstants.kt
+++ src/test/kotlin/com/rafaelbaetapena/archunitkotlin/architecture/PackagesConstants.kt
@@ -3,7 +3,7 @@ package com.rafaelbaetapena.archunitkotlin.architecture
 object PackagesConstants {
     const val configPackageName: String = "..config"
     const val corePackageName: String = "..core"
-    const val coreUseCasesPackageName: String = "..core.usecases"
+    const val coreUseCasesPackageName: String = "..core.usecases.."
     const val corePortsPackageName: String = "..core.ports"
     const val corePortsInPackageName: String = "..core.ports.in"
     const val corePortsOutPackageName: String = "..core.ports.out"
